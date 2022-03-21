1470d03df97630c150a97dacef850841e3f5173d
ucler <ucler@DESKTOP-25JG5UL>
2022-03-21 15:18:10 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+console.log("Merhaba")
+
+
+let dolarDun = 12.5;
+let dolarBugun = 13.5;
+
+if(dolarDun > dolarBugun);
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun);
+
+//Array - Diziler
+let konutKredileri = ["Konut Kredisi" , "Emlak Konut Kredisi" , "Kamu Konut Kredisi", "Yabancı Uyruklu Emlak Konut Kredisi" , "Büyük Fonlu Emlak Konut Kredisi"]
+
+
+for (let i =  0; i<konutKredileri.length; i++) {
+    console.log(konutKredileri[i])
+}
