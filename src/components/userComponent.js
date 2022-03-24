314edcbdafe9bf5f55d7813ebb5778596600ff5a
ucler <ucler@DESKTOP-25JG5UL>
2022-03-24 20:24:27 +0300

    Son Hali

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,4 +1,5 @@
 import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js"
+import Customer from "../models/customer.js"
 import User from "../models/user.js"
 import UserService from "../services/userService.js"
 
@@ -7,11 +8,21 @@ console.log("User Component Yüklendi")
 let logger1 = new MongoLogger()
 let userService = new UserService(logger1)
 
-let user1 = new User(1,"Üçler", "Gönültaş" , "Ankara")
-let user2 = new User(2,"Aynur", "Aydın" , "Ankara")
+let user1 = new User(1,"Üçler", "Gönültaş" , "Ankara", 27)
+let user2 = new User(2,"Aynur", "Aydın" , "Ankara", 25)
 userService.add(user1)
 userService.add(user2)
 
-console.log(userService.list())
-console.log(userService.getById(2))
+//console.log(userService.list())
+//console.log(userService.getById(2))
 
+console.log("------------------------------")
+userService.load()
+
+let customerToAdd = new Customer(1,"Ali", "Bektaş" , "Ankara", 25)
+customerToAdd.type = "customer"
+userService.add(customerToAdd)
+console.log(userService.costumers)
+console.log(userService.employees)
+console.log(userService.errors)
+console.log(userService.getCustomersSorted())
---
+++ src/data/users.js
@@ -0,0 +1,46 @@
+export const users = [
+    {
+        id: 1,
+        firstName: "Üçler",
+        lastName: "Gönültaş",
+        city:"Ankara",
+        age: 27,
+        CreditCartNumber: "592645123",
+        type: "customer"
+    },
+    {
+        id: 2,
+        firstName: "Aynur",
+        lastName: "Aydın",
+        city: "Ankara",
+        age: 21,
+        CreditCartNumber: "3123123123",
+        type: "customer"
+    },
+    {
+        id: 3,
+        firstName: "Nisa",
+        lastName: "Aydın",
+        city: "Simav",
+        
+        salary: 5000,
+        type: "employee"
+    },
+    {
+        id: 4,
+        firstName: "AAAA",
+        lastName: "AAAAA",
+        city: "AA",
+        age: 19,
+        salary: 2000,
+        type: "employee"
+    },
+    {
+        id: 5,
+        firstName: "John",
+        lastName: "White",
+        city: "New York",
+        age: 39,
+        type: "employe"
+    },
+]
